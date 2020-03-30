e1e8784c0c78be1f4ef54d175616b884704e4a34
Daniel Tipping <daniel@oldreliable.tech>
2020-03-30 15:03:55 +0100

    Autoformat worker

--- scripts/worker.py
+++ scripts/worker.py
@@ -12,19 +12,32 @@ from utils import save_data, save_metadata
 # Request handler #
 ###################
 
+
 def process_request(dc, s3_client, job_code, **kwargs):
     try:
         if job_code == "geomedian":
             from geomedian import process_geomedian
 
             ds = process_geomedian(dc=dc, **kwargs)
-            save_bands = ['red', 'green', 'blue', 'nir', 'swir1', 'swir2']
+            save_bands = ["red", "green", "blue", "nir", "swir1", "swir2"]
 
         if ds:
             logging.info("Saving data.")
-            save_data(s3_client=s3_client, ds=ds, job_code=job_code, bands=save_bands, **kwargs)
+            save_data(
+                s3_client=s3_client,
+                ds=ds,
+                job_code=job_code,
+                bands=save_bands,
+                **kwargs,
+            )
             logging.info("Saving metadata.")
-            save_metadata(s3_client=s3_client, ds=ds, job_code=job_code, bands=save_bands, **kwargs)
+            save_metadata(
+                s3_client=s3_client,
+                ds=ds,
+                job_code=job_code,
+                bands=save_bands,
+                **kwargs,
+            )
 
     except Exception as e:
         logging.error("Unhandled exception %s", e)
@@ -39,11 +52,12 @@ def process_request(dc, s3_client, job_code, **kwargs):
 # Job processor #
 #################
 
+
 def process_job(dc, dask_client, s3_client, json_data):
     loaded_json = json.loads(json_data)
 
     try:
-        #logging.info("Started processing job."))
+        # logging.info("Started processing job."))
         process_request(dc, s3_client, **loaded_json)
 
     except Exception as e:
@@ -58,8 +72,11 @@ def process_job(dc, dask_client, s3_client, json_data):
 # Worker #
 ##########
 
+
 def worker():
-    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(message)s", level=logging.INFO
+    )
 
     host = os.getenv("REDIS_SERVICE_HOST", "redis-master")
     q = rediswq.RedisWQ(name="jobProduct", host=host)
@@ -74,16 +91,17 @@ def worker():
     s3_client = S3Client()
 
     while not q.empty():
-      item = q.lease(lease_secs=1800, block=True, timeout=600)
-      if item is not None:
-        itemstr = item.decode("utf=8")
-        logging.info("Working on %s.", itemstr)
-        process_job(dc, dask_client, s3_client, itemstr)
-        q.complete(item)
-      else:
-        logging.info("Waiting for work.")
+        item = q.lease(lease_secs=1800, block=True, timeout=600)
+        if item is not None:
+            itemstr = item.decode("utf=8")
+            logging.info("Working on %s.", itemstr)
+            process_job(dc, dask_client, s3_client, itemstr)
+            q.complete(item)
+        else:
+            logging.info("Waiting for work.")
 
     logging.info("Queue empty, exiting.")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     worker()
